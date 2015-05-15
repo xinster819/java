5283700f605a2aa275a57644d92eee8cef7f457c
alchemistli <alchemistli@sohu-inc.com>
2015-05-15 18:28:59 +0800

    dd

--- lx.springmvc/src/main/java/lx/springmvc/controller/TestController.java
+++ lx.springmvc/src/main/java/lx/springmvc/controller/TestController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-@RequestMapping("/api")
 @Controller
 public class TestController {
 
