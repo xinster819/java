92ce3a74c971bad70d67d67dcc36d01b380af733
alchemistli <alchemistli@sohu-inc.com>
2015-05-15 18:07:37 +0800

    test

---
+++ lx.springmvc/src/main/java/lx/springmvc/controller/TestController.java
@@ -0,0 +1,42 @@
+package lx.springmvc.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping("/api")
+@Controller
+public class TestController {
+
+    @RequestMapping("/dd")
+    @ResponseBody
+    public String test() {
+        return "dd";
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                InputStream is = file.getInputStream();
+                InputStreamReader r = new InputStreamReader(is);
+                BufferedReader reader = new BufferedReader(r);
+                while (reader.ready()) {
+                    System.out.println(reader.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "dd";
+    }
+}
