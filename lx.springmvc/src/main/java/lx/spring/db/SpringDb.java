9e9084aa78acf8a9e79a8ab414c5f5befc728a12
alchemistli <alchemistli@sohu-inc.com>
2015-06-09 17:41:43 +0800

    f

--- lx.springmvc/src/main/java/lx/spring/db/SpringDb.java
+++ lx.springmvc/src/main/java/lx/spring/db/SpringDb.java
@@ -2,17 +2,24 @@ package lx.spring.db;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SpringDb {
 
+    static Logger logger = LoggerFactory.getLogger(SpringDb.class);
+
     @Resource
     JdbcTemplate jdbcTemplate;
-    
+
+    @SuppressWarnings("deprecation")
     public long go() {
-        return jdbcTemplate.queryForLong("select count(*) from agency_daily_record");
+        logger.info("info lx");
+        logger.debug("debug lx");
+        return jdbcTemplate.queryForLong("select count(*) from labs_stat");
     }
 
 }
