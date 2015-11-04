4b1b97b116a6067d9f748d8c67b2220694b6ada7
alchemistli <alchemistli@sohu-inc.com>
2015-11-04 15:44:30 +0800

    update deploy sh

--- lx.springmvc/deploy/deploy.sh
+++ lx.springmvc/deploy/deploy.sh
@@ -17,14 +17,6 @@ fi
 version=$("$java_home" -version 2>&1 | awk -F '"' '/version/ {print $2}')
 echo jdk version "$version"
 
-name=${name}
-base="/opt/cmt/demo"
-
-data_home="/opt/data"
-log_home="/opt/logs/resin"
-bin_home="/opt/bin/resin"
-conf_home="/opt/conf/resin"
-
 servers=(${resin.server})
 
 #
@@ -34,19 +26,20 @@ i=0
 while [ $i -lt $serverCount ]
 do
     server=${servers[$i]}
-    echo "deploy $name on $server"
+    echo "deploy ${name} on $server"
     host=`echo $server | cut -d: -f1`
     port=`echo $server | cut -d: -f2`
 
-    log="${log_home}/$name-$port"
-    conf="${conf_home}/$name-$port.xml"
-    deploy="$base/$name/$port"
+    log="${log.path}/${name}-$port"
+    conf="${conf.path}/${name}-$port.xml"
+    data="${data.path}/$name/$port"
+    deploy="${deploy.path}/${name}/$port"
     webapp="$deploy/webapp"
-    shell="${bin_home}/$name-$port.sh";
+    shell="${bin.path}/$name-$port.sh";
 
     #create deploy dir
-    ssh root@$host "mkdir -p ${data_home}/$name/$port"
-    ssh root@$host "chown -R resin ${data_home}/$name/$port"
+    ssh root@$host "mkdir -p $data"
+    ssh root@$host "chown -R resin $data"
     ssh root@$host "mkdir -p $log"
     ssh root@$host "chown -R resin $log"
     
@@ -59,33 +52,33 @@ do
     done
     
     #process shell script
-    ssh root@$host "mkdir -p ${bin_home}"
-    scp start.sh root@$host:${bin_home}
-    ssh root@$host "chmod +x ${bin_home}/start.sh"
-    ssh root@$host "mv -f ${bin_home}/start.sh $shell"
+    ssh root@$host "mkdir -p ${bin.path}"
+    scp start.sh root@$host:${bin.path}
+    ssh root@$host "chmod +x ${bin.path}/start.sh"
+    ssh root@$host "mv -f ${bin.path}/start.sh $shell"
     ssh root@$host "sed -i 's/##conf##/${conf//\//\\/}/' $shell"
     ssh root@$host "chown resin $shell"
 
     #process config file, maybe cause resin restart
-    ssh root@$host "mkdir -p ${conf_home}"
-    ssh root@$host "chown -R resin ${conf_home}"
-    scp resin4.xml resin@$host:${conf_home}
-    ssh resin@$host "mv -f ${conf_home}/resin4.xml $conf"
+    ssh root@$host "mkdir -p ${conf.path}"
+    ssh root@$host "chown -R resin ${conf.path}"
+    scp resin4.xml resin@$host:${conf.path}
+    ssh resin@$host "mv -f ${conf.path}/resin4.xml $conf"
     ssh resin@$host "sed -i 's/##port##/$port/' $conf"
 
     #unpackage war
     ssh root@$host "mkdir -p $webapp"
     ssh root@$host "chown -R resin $deploy"
     
-    scp ../../$name.war resin@$host:$deploy
-    ssh resin@$host "cd $webapp && rm -rf * && /opt/apps/jdk/bin/jar xf $deploy/$name.war"
-    ssh root@$host "cp $deploy/$name.war $deploy/$name-${serial_no}.war"
+    scp ../../${name}.war resin@$host:$deploy
+    ssh resin@$host "cd $webapp && rm -rf * && /opt/apps/jdk/bin/jar xf $deploy/${name}.war"
+    ssh root@$host "cp $deploy/${name}.war $deploy/${name}-${serial_no}.war"
     
     #create soft link for data resources
-    ssh resin@$host "rm -f ${data_home}/$name/js && ln -s $webapp/js ${data_home}/$name/js"
-    ssh resin@$host "rm -f ${data_home}/$name/css && ln -s $webapp/css ${data_home}/$name/css"
-    ssh resin@$host "rm -f ${data_home}/$name/img && ln -s $webapp/img ${data_home}/$name/img"
-    ssh resin@$host "rm -f ${data_home}/$name/sample && ln -s $webapp/sample ${data_home}/$name/sample"
+    ssh resin@$host "rm -f ${data.path}/${name}/js && ln -s $webapp/js ${data.path}/${name}/js"
+    ssh resin@$host "rm -f ${data.path}/${name}/css && ln -s $webapp/css ${data.path}/${name}/css"
+    ssh resin@$host "rm -f ${data.path}/${name}/img && ln -s $webapp/img ${data.path}/${name}/img"
+    ssh resin@$host "rm -f ${data.path}/${name}/sample && ln -s $webapp/sample ${data.path}/${name}/sample"
 
     ssh resin@$host "$shell start"
     
