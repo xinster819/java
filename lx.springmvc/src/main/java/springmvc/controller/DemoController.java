75e9d50c963cc95b9bc80905bb81caf86cd4f607
alchemistli <alchemistli@sohu-inc.com>
2015-11-17 11:27:55 +0800

    add redis cluster

--- lx.springmvc/src/main/java/springmvc/controller/DemoController.java
+++ lx.springmvc/src/main/java/springmvc/controller/DemoController.java
@@ -1,12 +1,16 @@
 package springmvc.controller;
 
+import java.util.Set;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import redis.clients.jedis.JedisCluster;
 import springmvc.service.DemoService;
 
 @Controller
@@ -16,11 +20,32 @@ public class DemoController {
     @Resource
     private DemoService demoService;
 
+    @Resource
+    private JedisCluster jedisCluster;
+
     @RequestMapping("show/{id}")
     public ModelAndView show(@PathVariable("id") int id) {
         ModelAndView mv = new ModelAndView();
         mv.addObject("name", demoService.byId(id));
         mv.setViewName("show");
         return mv;
+    }
+
+    @RequestMapping("jedis")
+    @ResponseBody
+    public String jedis() {
+        int i = 0;
+        while (i < 1000000000) {
+            jedisCluster.set(String.valueOf(i), String.valueOf(i));
+            i++;
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println(i);
+        }
+        return "ok";
     }
 }
